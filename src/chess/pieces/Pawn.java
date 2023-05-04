975256f4b87642936d2277202532e6bf40ac05bc
Mauricio <mauricioittner30@gmail.com>
2023-05-04 13:04:01 -0300

    Bishop

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -31,17 +31,19 @@ public class Pawn extends ChessPiece {
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
-			p.setValues(position.getRow() + 1, position.getColumn() + 1);
+			p.setValues(position.getRow() - 1, position.getColumn() + 1);
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
 				} 
+			}
 				else {
+
 					p.setValues(position.getRow() + 1, position.getColumn());
 					if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 						mat[p.getRow()][p.getColumn()] = true;
 					}
 					p.setValues(position.getRow() + 2, position.getColumn());
-					p2 = new Position(position.getRow() + 1, position.getColumn());
+					Position p2 = new Position(position.getRow() + 1, position.getColumn());
 					if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2)
 							&& !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
 						mat[p.getRow()][p.getColumn()] = true;
@@ -54,11 +56,10 @@ public class Pawn extends ChessPiece {
 					if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 						mat[p.getRow()][p.getColumn()] = true;
 					}
-
-			}
-		}
+				}
 		return mat;
-	}
+		}
+		
 
 	@Override
 	public String toString() {
